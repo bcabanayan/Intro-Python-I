00d6e75e39751fbf1b3efee6569f55b90ae94418
bcabanayan <bcabanayan@gmail.com>
2019-05-12 20:45:34 -0700

    Completed file io exercises

--- src/file_io.py
+++ src/file_io.py
@@ -10,9 +10,19 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+f = open('foo.txt')
+print(f.read())
+f.close()
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+bar = open('bar.txt', 'w')
+bar.write('hi\n')
+bar.write('this is a test\n')
+bar.write('hope it works!\n')
+bar.close()
